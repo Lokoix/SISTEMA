b70381825bbb746eb67c3439c78f0ffb83cac55a
gbrProgrammer <39705341+gbrProgrammer@users.noreply.github.com>
2018-05-28 14:33:16 -0300

    dsasa

--- SistemaSe7i/src/Beans/UsuariosBeans.java
+++ SistemaSe7i/src/Beans/UsuariosBeans.java
@@ -5,14 +5,11 @@
  */
 package Beans;
 
-public class LoteBeans {
-    int id;
-    ProprietarioBeans proprietario;
-<<<<<<< HEAD
-    int carro;
-=======
-    int dcasa;
->>>>>>> d03dbccdc571110b0cfa2bc95e7873e423d2d679
-
+/**
+ *
+ * @author Eu
+ */
+public class UsuariosBeans {
+    
     
 }
