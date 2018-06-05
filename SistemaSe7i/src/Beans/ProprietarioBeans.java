ab7c1e35c50dced9213f748d6111d4aa002ab90e
gbrProgrammer <39705341+gbrProgrammer@users.noreply.github.com>
2018-06-05 10:29:24 -0300

    Comitado

---
+++ SistemaSe7i/src/Beans/ProprietarioBeans.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author Eu
+ */
+public class ProprietarioBeans {
+    
+    private int id;
+    private String cpfCpnpj;
+    private String nome;
+    private String endereco;
+    private String bairro;
+    private String cep;
+    private CidadeBeans cidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpfCpnpj() {
+        return cpfCpnpj;
+    }
+
+    public void setCpfCpnpj(String cpfCpnpj) {
+        this.cpfCpnpj = cpfCpnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public CidadeBeans getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(CidadeBeans cidade) {
+        this.cidade = cidade;
+    }
+    
+    
+}
